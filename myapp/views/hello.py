bac2d14786cf478afd752512a70491ac4c71e736
vagosalv <vagosalvgd@gmail.com>
2018-01-24 20:21:05 +0200

    Update hello.py

--- myapp/views/hello.py
+++ myapp/views/hello.py
@@ -5,13 +5,33 @@ from ..app import mysql
 
 hello = Blueprint('hello',__name__)
 
-
+arxikh
 @hello.route('/')
 def index_page():
+	return render_template('home.html')
 
-	
-	c = mysql.db.cursor()
-	c.execute('SELECT * FROM employee')
-	results = c.fetchall()
-	return render_template('index.html',results=results)
+#About
+@hello.route('/about')
+def about():
+    return render_template('about.html')
+
+
+#Articles
+@hello.route('/articles')
+def articles():
+    #Create cursor
+    c = mysql.db.cursor()
+    #Get Articles
+    result = c.execute("SELECT * FROM articles")
+    articles = c.fetchall()
+    if result > 0 :
+        return render_template('articles.html', articles=articles)
+    else:
+        msg = 'No articles Found'
+        return render_template('articles.html', msg=msg)
+    #close connection
+    c.close()
+
+
+
 
