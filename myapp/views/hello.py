95e9da7b01ae46cf053ac0a79fb54235631ac505
vagosalv <vagosalvgd@gmail.com>
2018-01-25 19:22:37 +0200

    Update hello.py

--- myapp/views/hello.py
+++ myapp/views/hello.py
@@ -1,4 +1,4 @@
-from flask import Blueprint,render_template,flash,url_for,session,request,logging,redirect
+from flask import Blueprint, render_template, flash, url_for, session, request, logging, redirect, flask
 from wtforms import Form, StringField, TextAreaField, PasswordField, validators
 from passlib.hash import sha256_crypt
 from functools import wraps
