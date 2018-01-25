e3a935ac678c33ec560d7e3a0928328ccfec2d75
vagosalv <vagosalvgd@gmail.com>
2018-01-25 18:48:52 +0200

    Update hello.py

--- myapp/views/hello.py
+++ myapp/views/hello.py
@@ -1,6 +1,6 @@
-from flask import Blueprint,render_template,flash,url_for
+from flask import Blueprint,render_template,flash,url_for,session,request,logging
 from wtforms import Form, StringField, TextAreaField, PasswordField, validators
-
+from passlib.hash import sha256_crypt
 from ..app import mysql
 
 
@@ -92,3 +92,44 @@ def register():
 
         return redirect(url_for('login'))
     return render_template('register.html', form = form)
+
+
+#User login
+@hello.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        #Get form fields
+        username = request.form['username']
+        password_candidate = request.form['password']
+        #Create cursor
+        c = mysql.db.cursor()
+        #Get user by username
+        result = c.execute("SELECT * FROM users WHERE username = %s" , [username])
+
+        if result > 0:
+            #Get stored hash
+            data = c.fetchone()
+            password = data['password']
+
+            #Compare Passwords
+            if sha256_crypt.verify(password_candidate, password):
+                #Passed
+                session['logged_in'] = True
+                session['username'] = username
+
+                flash('You are now logged in', 'success')
+                return redirect(url_for('dashboard'))
+            else:
+                error = 'Invalid login'
+                return render_template('login.html', error=error )
+            #close connection
+            c.close()
+        else:
+            error = 'Username not found'
+            return render_template('login.html', error=error )
+
+
+    return render_template('login.html')
+
+
+
