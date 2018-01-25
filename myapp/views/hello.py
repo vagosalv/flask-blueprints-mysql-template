53f1d2e9e1dbddd3b1acb83f26c46be196037ed2
vagosalv <vagosalvgd@gmail.com>
2018-01-25 18:35:52 +0200

    Update hello.py

--- myapp/views/hello.py
+++ myapp/views/hello.py
@@ -50,7 +50,16 @@ def article(id):
     return render_template('article.html', article=article)
 
 
-
+#klash gia elenxo ths formas
+class RegisterForm(Form):
+    name = StringField('Name', [validators.Length(min=1, max=50)])
+    username = StringField('Username', [validators.Length(min=4, max=25)])
+    email = StringField('Email', [validators.Length(min=6, max=50)])
+    password = PasswordField('Password', [
+        validators.DataRequired(),
+        validators.EqualTo('confirm', message='Passwords do not match')
+    ])
+    confirm = PasswordField('Confirm Password')
 
 
 
