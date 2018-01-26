131d5bc1470f95029b6eae53ed4c6bbc8cc6fa3f
vagosalv <vagosalvgd@gmail.com>
2018-01-26 20:33:08 +0200

    Update hello.py

--- myapp/views/hello.py
+++ myapp/views/hello.py
@@ -46,7 +46,6 @@ def article(id):
     #Get Article
     result = c.execute("SELECT * FROM articles WHERE id = %s", [id])
     #Commit
-    show_comment(id)#auto mphke
     article = c.fetchone()
     return render_template('article.html', article=article)
 
