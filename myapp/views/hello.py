6f4fcfc0e9788da949dc8c71484b2adfc8063b58
vagosalv <vagosalvgd@gmail.com>
2018-01-29 19:18:12 +0200

    Update hello.py

--- myapp/views/hello.py
+++ myapp/views/hello.py
@@ -18,7 +18,7 @@ def test(id):
     #Create cursor
     c = mysql.db.cursor()
     #Get Article
-    result = c.execute("SELECT * FROM articles LEFT JOIN comments ON comments.article_id = articles.id and articles.id = %s ", [id])
+    result = c.execute("SELECT * FROM articles LEFT JOIN comments ON articles.id = %s and comments.article_id = articles.id  ", [id])
     #Commit
     article = c.fetchone()
     return render_template('test.html', article=article)
