4170966ab1d6ad214a9ed2c60bb6612bb63266f4
vagosalv <vagosalvgd@gmail.com>
2018-05-17 00:06:39 +0300

    Update comments.py

--- myapp/views/comments.py
+++ myapp/views/comments.py
@@ -7,14 +7,14 @@ from ..app import mysql
 from .users import is_logged_in
 
 
-com = Blueprint('com',__name__)
+hello = Blueprint('hello',__name__)
 
 #Commnts Form class
 class CommentForm(Form):
     body = TextAreaField('Body', [validators.Length(min=10)])
 
 #Comments
-@com.route('/comments')
+@hello.route('/comments')
 def comments():
     #Create cursor
     c = mysql.db.cursor()
@@ -33,7 +33,7 @@ def comments():
 
 
 #Add comment
-@com.route('/add_comments/<string:article_id>', methods=['GET', 'POST'])
+@hello.route('/add_comments/<string:article_id>', methods=['GET', 'POST'])
 @is_logged_in
 def add_comment(article_id):
 
@@ -55,7 +55,7 @@ def add_comment(article_id):
 
 
 #Edit comment
-@com.route('/edit_comment/<string:id>', methods=['GET', 'POST'])
+@hello.route('/edit_comment/<string:id>', methods=['GET', 'POST'])
 @is_logged_in
 def edit_comment(id):
     #create cursor
@@ -84,7 +84,7 @@ def edit_comment(id):
     return render_template('edit_comment.html', form=form)
 
 #Delete comment
-@com.route('/delete_comment/<string:id>', methods=['POST'])
+@hello.route('/delete_comment/<string:id>', methods=['POST'])
 @is_logged_in
 def delete_comment(id):
     #create cursor
