f4247c17baa2c3f1ac94e074be1b6d3448b95fe2
vagosalv <vagosalvgd@gmail.com>
2018-09-21 18:27:48 +0300

    Update articles.py

--- myapp/views/articles.py
+++ myapp/views/articles.py
@@ -62,7 +62,7 @@ def article(id):
 		result = c.execute("SELECT * FROM articles WHERE id = %s", [id])	
 
 	#Commit
-	article = c.fetchone()
+	article = c.fetchall()
 	return render_template('article.html', article=article)
 	
 	
